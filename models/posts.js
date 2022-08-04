042f7569081d95db976c0929426b29dd6ef35b02
Anik Ghosh <anikghosh46.bd@gmail.com>
2022-08-04 15:21:31 +0600

    Post model added.

---
+++ models/posts.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Posts.init({
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Posts',
+  });
+  return Posts;
+};
