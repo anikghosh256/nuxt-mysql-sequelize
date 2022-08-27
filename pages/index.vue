84cb706c27af92bdbe9b1bedb4ce0b6d9ab08969
Anik Ghosh <anikghosh46.bd@gmail.com>
2022-08-27 13:16:33 +0600

    Working on crud functionality.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,97 @@
+<script setup>
+import { ref, reactive, watchEffect } from 'vue'
+// get posts from api
+const { data: posts, pending, refresh, error  } = await useFetch('/api/posts');
+
+// reactive form data properties
+const form = reactive({
+  id: '',
+  content: '',
+  error: false
+});
+
+const update = ref(false);
+
+watchEffect(() => {
+  if (form.content.length > 0) {
+    form.error = false;
+  } else {
+    form.id = '';
+    update.value = false;
+  }
+  if(form.id != '') {
+    update.value = true
+  }
+});
+
+async function post(event) {
+  event.preventDefault();
+
+  // validate form data
+  form.error = false;
+  if (!form.content.trim()) {
+    form.error = true;
+    return;
+  }
+
+  // post form data to api
+  await $fetch('/api/posts', {
+    method: 'POST',
+    body: JSON.stringify(form)
+  });
+  refresh();
+  // reset form data
+  form.id = '';
+  form.content = '';
+}
+// Select for update
+async function select({id, title, content}) {
+  form.id = id;
+  form.content = title +' || '+content;
+}
+
+// Delete post
+async function deletePost(id) {
+  await $fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+  refresh();
+}
+
+
+
+
+
+useHead(() => {
+  return {
+    title: 'Hello World',
+    meta: [
+      {
+        name: 'description',
+        content: 'This is a crud.'
+      }
+    ]
+  }
+})
+</script>
+
 <template>
   <div class="container row justify-content-center">
     <div class="my-5 col-12 col-md-8 border border-light rounded px-0">
       <div class="border-bottom border-light mb-4">
         <!--single line form-->
-        <form action="{{ route('search') }}" method="GET" class="row">
+        <form @submit="post" class="row">
           <div class="form-group col">
             <input
               type="text"
               name="content"
-              class="form-control"
+              v-model.trim="form.content"
+              class="form-control" :class="form.error ? 'is-invalid ' : ''"
               placeholder="Title || Content"
             />
           </div>
           <div class="col-2 mr-0">
-            <button type="submit" class="btn btn-primary w-100">Add</button>
+            <button type="submit" class="btn btn-primary w-100">{{ update ? "Update" : "Add" }}</button>
           </div>
         </form>
       </div>
@@ -27,31 +105,13 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row" class="align-middle">1</th>
-            <td class="align-middle">Mark</td>
-            <td class="align-middle">Otto</td>
+          <tr v-for="post in posts" :key="post.id">
+            <th scope="row" class="align-middle">{{ post.id }}</th>
+            <td class="align-middle">{{ post.title }}</td>
+            <td class="align-middle">{{ post.content }}</td>
             <td>
-              <button class="btn btn-primary">Edit</button>
-              <button class="btn btn-danger ms-3">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" class="align-middle">2</th>
-            <td class="align-middle">Mark</td>
-            <td class="align-middle">Otto</td>
-            <td>
-              <button class="btn btn-primary">Edit</button>
-              <button class="btn btn-danger ms-3">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" class="align-middle">3</th>
-            <td class="align-middle">Mark</td>
-            <td class="align-middle">Otto</td>
-            <td>
-              <button class="btn btn-primary">Edit</button>
-              <button class="btn btn-danger ms-3">Delete</button>
+              <button class="btn btn-primary" @click="select(post)">Edit</button>
+              <button class="btn btn-danger ms-3" @click="deletePost(post.id)">Delete</button>
             </td>
           </tr>
         </tbody>
---
+++ server/api/posts.post.js
@@ -0,0 +1,50 @@
+import db from '../../models/index.js';
+
+export default defineEventHandler(async (event) => {
+  const form = await useBody(event);
+  const { title, content } = extractTitleAndBody(form.content);
+
+  if (form.id && form.id != '') {
+    return updatePost(form.id, title, content);
+  }
+  return createPost(title, content);
+})
+
+
+/**
+ * Extract title and body from content.
+ * 
+ * @param {String} content - The content of the post.
+ * @returns {Object} - The title and body of the post.
+ */
+function extractTitleAndBody(bodyData) {
+  const dataArr = bodyData.split('||');
+  const title = dataArr[0].trim();
+  const content = (dataArr[1] || '').trim();
+  return { title, content };
+}
+
+/**
+ * Create a new post.
+ * 
+ */
+async function createPost(title, content) {
+  const post = await db.Posts.create({
+    title,
+    content,
+  });
+  return post;
+}
+
+/**
+ * Update a post.
+ */
+async function updatePost(id, title, content) {
+  const post = await db.Posts.update({
+    title,
+    content,
+  }, {
+    where: { id },
+  });
+  return post;
+}
