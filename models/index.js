d53ce2067e8ff6ed006edec7842a9ccb0053a672
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-04-19 23:39:33 +0600

    ESM issue fixed.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+import Sequelize, { DataTypes } from 'sequelize';
+import allConfig from '~~/config/config.json';
+
+
+// import all models
+import Post from './posts.js';
+
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = allConfig[env];
+
+
+
 const db = {};
 
 let sequelize;
@@ -15,15 +21,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+db['Posts'] = Post(sequelize, DataTypes);
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
@@ -34,4 +32,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
--- models/posts.js
+++ models/posts.js
@@ -1,8 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
   class Posts extends Model {
     /**
      * Helper method for defining associations.
